a67e002c0e1f85b5b022d163c3556b5961760f44
S <s>
2015-04-09 15:25:22 +0300

    {"type":"TI", "runs":"1"}

--- NameSurferEntry.java
+++ NameSurferEntry.java
@@ -30,14 +30,14 @@ public class NameSurferEntry implements NameSurferConstants {
 		String numbers = line.substring(nameEnd + 1);
 		String startParsing = numbers;
 //		println("startParsing:" + startParsing);
-		for (int i=1; i<NDECADES; i++) {
+		for (int i=1; i<NDECADES + 1; i++) {
 			int endDecade = startParsing.indexOf(" ");
 //			println("endDecade:"+endDecade);
 			String decade = startParsing.substring(0, endDecade);
 //			println("decade:" + decade);
 			int decadeInt = Integer.parseInt(decade);
 //			println("decadeInt" + decadeInt);
-			rankings[i-1] = decadeInt;
+			rankings[i] = decadeInt;
 //			println(rankings[i]);
 			startParsing = startParsing.substring(endDecade + 1);
 //			println("new start parsing:" + startParsing);
