f13e3d59d6268813c339ebfd7db9e2e5147aebd3
S <s>
2015-04-09 15:38:48 +0300

    {"type":"TI", "runs":"1"}

--- NameSurferEntry.java
+++ NameSurferEntry.java
@@ -77,8 +77,9 @@ public class NameSurferEntry implements NameSurferConstants {
  * NameSurferEntry.
  */
 	public String toString() {
+		toString = name;
 		for (int i=0; i<NDECADES; i++) {
-			toString = "" + rankings[i];
+			toString += rankings[i];
 		}
 		return toString;
 	}
