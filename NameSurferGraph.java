cf3c563d34c3fb5fab083bc64a70423de944f31b
S <s>
2015-04-09 17:37:36 +0300

    {"type":"TI", "runs":"1"}

--- NameSurferGraph.java
+++ NameSurferGraph.java
@@ -94,7 +94,7 @@ public class NameSurferGraph extends GCanvas
 		
 		GLine upperLine = new GLine (0, GRAPH_MARGIN_SIZE, APPLICATION_WIDTH,GRAPH_MARGIN_SIZE );
 		
-		GLine lowerLine = new GLine (0, 100,APPLICATION_WIDTH ,100 );
+		GLine lowerLine = new GLine (0, APPLICATION_HEIGHT,APPLICATION_WIDTH ,100 );
 		
 		add (oneLine);
 		add (twoLine);
