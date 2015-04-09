0628453ee8b746e23751303146b8d05152fa1632
S <s>
2015-04-09 17:11:39 +0300

    {"type":"TI", "runs":"2"}

--- NameSurfer.java
+++ NameSurfer.java
@@ -20,16 +20,11 @@ public class NameSurfer extends Program implements NameSurferConstants {
 	    // You fill this in, along with any helper methods //
 		addInteractors();
 		addActionListeners();
-		String line = "Abraham 144 158 261 350 408 410 503 347 274 238 214";
-		NameSurferEntry entry = new NameSurferEntry(line);
+//		String line = "Abraham 144 158 261 350 408 410 503 347 274 238 214";
+//		NameSurferEntry entry = new NameSurferEntry(line);
 		
-		println(entry.getName());
-		for (int i=0; i<11; i++) {
-			println(entry.getRank(i));		
-		}
-		
-		println("To String:" + entry.toString());
-		
+//		println(entry.getName());
+
 		
 		
 	}
